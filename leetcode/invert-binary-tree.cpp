222898587520a1c0a3e994bb383180dd32f11764
Qasim Zeeshan <qasim.zeeshan@coeus-solutions.de>
2015-10-28 18:00:14 +0500

    adding invert binary tree

---
+++ leetcode/invert-binary-tree.cpp
@@ -0,0 +1,14 @@
+//solution to: https://leetcode.com/problems/invert-binary-tree/
+class Solution {
+  public:
+    TreeNode* invertTree(TreeNode* root) {
+      if (root == NULL)
+        return NULL;
+      TreeNode* temp = root->left;
+      root->left = root->right;
+      root->right = temp;
+      invertTree(root->left);
+      invertTree(root->right);
+      return root;
+    }
+};
