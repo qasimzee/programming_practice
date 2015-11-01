0c6012733da1cf17c777b625b9f3b18133dbcf8e
Qasim Zeeshan <qasim.zeeshan@coeus-solutions.de>
2015-11-01 15:35:49 +0500

    adding excel column

---
+++ leetcode/excel-sheet.cpp
@@ -0,0 +1,32 @@
+i//https://leetcode.com/problems/excel-sheet-column-number/
+//https://leetcode.com/problems/excel-sheet-column-number/
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+  public:
+    int titleToNumber(string s) {
+      int n = s.size();
+      int ans = 0;
+      reverse(s.begin(), s.end());
+      for (int i = 0; i < n; i++) {
+        int power = pow(26, i);
+        ans += (s[i] - 'A' + 1) * power;
+      }
+      return ans;
+    }
+};
+
+int main() {
+  Solution s;
+  cout << s.titleToNumber("A");
+  return 0;
+}
+
+
+
