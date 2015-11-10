e81dd5df06dac3d45805ea2abd86d17becb8b924
Qasim Zeeshan <qasim.zeeshan@coeus-solutions.de>
2015-11-10 12:41:54 +0500

    adding skii map

--- redmart/skii.cpp
+++ redmart/skii.cpp
@@ -23,66 +23,59 @@ using namespace std;
 class RedmartSkii
 {
     vector<vector<int>> skii_map;
-    int temp[1000][1000];
+    int paths[1000][1000];
     int drop[1000][1000];
     int N;  //The size of grid
+    
+    //A depth first search function.
     int fill(int i, int j)
     {
         if (i < 0 || j < 0 || i >= N || j >= N)
             return 0;
-        if (temp[i][j] != 0)
-            return temp[i][j];
+        if (paths[i][j] != 0)
+            return paths[i][j];
         
         int max_direction = 0, direction;
-        int max_i = -1, max_j  = -1;
         if (i - 1 >= 0 && skii_map[i - 1][j] < skii_map[i][j])
         {
             direction = fill(i - 1, j);
-            if (direction > max_direction)
+            if (direction >= max_direction)
             {
-                max_i = i - 1;
-                max_j = j;
                 max_direction = direction;
+                drop[i][j] = max(skii_map[i][j] - skii_map[i - 1][j] + drop[i - 1][j], drop[i][j]);
             }
             
         }
         if (j - 1 >= 0 && skii_map[i][j - 1] < skii_map[i][j])
         {
             direction = fill(i, j - 1);
-            if (direction > max_direction)
+            if (direction >= max_direction)
             {
-                max_i = i;
-                max_j = j - 1;
                 max_direction = direction;
+                drop[i][j] = max(skii_map[i][j] - skii_map[i][j - 1] + drop[i][j - 1], drop[i][j]);
             }
         }
         
         if (i + 1 < N && skii_map[i + 1][j] < skii_map[i][j])
         {
             direction = fill(i + 1, j);
-            if (direction > max_direction)
+            if (direction >= max_direction)
             {
-                max_i = i + 1;
-                max_j = j;
                 max_direction = direction;
+                drop[i][j] = max(skii_map[i][j] - skii_map[i + 1][j] + drop[i + 1][j], drop[i][j]);
             }
         }
         if (j + 1 < N && skii_map[i][j + 1] < skii_map[i][j])
         {
             direction = fill(i, j + 1);
-            if (direction > max_direction)
+            if (direction >= max_direction)
             {
-                max_i = i;
-                max_j = j + 1;
                 max_direction = direction;
+                drop[i][j] = max(skii_map[i][j] - skii_map[i][j + 1] + drop[i][j + 1], drop[i][j]);
             }
         }
-        temp[i][j] = max_direction + 1;
-        if (temp[i][j] == 1)
-            drop[i][j] = 0;
-        else
-            drop[i][j] = skii_map[i][j] - skii_map[max_i][max_j] + drop[max_i][max_j];
-        return temp[i][j];
+        paths[i][j] = max_direction + 1;
+        return paths[i][j];
     }
     void findDistances()
     {
@@ -90,7 +83,7 @@ class RedmartSkii
         {
             for (int j = 0; j < N; j++)
             {
-                if (temp[i][j] == 0)
+                if (paths[i][j] == 0)
                 {
                     fill(i, j);
                 }
@@ -102,7 +95,7 @@ public:
     RedmartSkii(int N, vector<vector<int>> skii_map)
     {
         this->skii_map = skii_map;
-        memset(temp, 0, sizeof(temp[0][0]) * 1000 * 1000);
+        memset(paths, 0, sizeof(paths[0][0]) * 1000 * 1000);
         memset(drop, 0, sizeof(drop[0][0]) * 1000 * 1000);
         this->N = N;
     }
@@ -115,12 +108,12 @@ public:
         {
             for (int j = 0; j < N; j++)
             {
-                if (temp[i][j] > longest_path)
+                if (paths[i][j] > longest_path)
                 {
-                    longest_path = temp[i][j];
+                    longest_path = paths[i][j];
                     max_drop = drop[i][j];
                 }
-                else if (temp[i][j] == longest_path)
+                else if (paths[i][j] == longest_path)
                 {
                     if (drop[i][j] > max_drop)
                     {
@@ -128,7 +121,7 @@ public:
                     }
                 }
             }
-
+            
         }
         stringstream ss;
         ss << longest_path <<" "<< max_drop;
@@ -150,11 +143,11 @@ public:
         {
             for (int j = 0; j < N; j++)
             {
-                cout << temp[i][j] << " ";
+                cout << paths[i][j] << " ";
             }
             cout << endl;
         }
-
+        
         cout << endl;
         for (int i = 0; i < N; i++)
         {
@@ -170,7 +163,7 @@ public:
 
 int main() {
     ifstream cin;
-    cin.open("/Users/qasimzeeshan/Documents/cpp/TC/TC/map.txt", ios::in);
+    cin.open("/file/to/path", ios::in);
     int N;
     vector<vector<int>> skii_map;
     
@@ -188,9 +181,6 @@ int main() {
         skii_map.push_back(v);
     }
     RedmartSkii *r = new RedmartSkii(N, skii_map);
-    
     cout << r->findSteep();
-//    r->print();
     return 0;
 }
-
