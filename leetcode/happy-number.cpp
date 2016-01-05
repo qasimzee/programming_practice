b5b4767cb687bf771b16803db9f551c9818316a6
Qasim Zeeshan <newbillgates@gmail.com>
2016-01-05 09:47:45 +0500

    fixing

---
+++ leetcode/happy-number.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+
+class Solution {
+  public:
+    int getSum(int x) {
+      int sum = 0;
+      while (x != 0) {
+        sum += ((x % 10) * (x % 10));
+        x /= 10;
+      }
+      return sum;
+    }
+    bool isHappy(int n) {
+      unordered_map<int, bool> map;
+      map[n] = true;
+      while (n != 1) {
+        n = getSum(n);
+        if (map[n]) {
+          return false;
+        }
+        map[n] = true;
+      }
+      return true;
+    }
+};
+
+int main() {
+  Solution s;
+  cout << s.isHappy(7);
+  return 0;
+}
+
+
