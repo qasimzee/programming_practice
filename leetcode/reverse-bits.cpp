9e712e54abacfc066e51f40ec462ea4d49eb1b0f
Qasim Zeeshan <newbillgates@gmail.com>
2016-01-26 16:36:57 +0500

    adding https://leetcode.com/problems/reverse-bits/

---
+++ leetcode/reverse-bits.cpp
@@ -0,0 +1,13 @@
+//Solution to https://leetcode.com/problems/reverse-bits/
+class Solution {
+  public:
+    uint32_t reverseBits(uint32_t n) {
+      uint32_t x = 0;
+      for (int i = 0; i < 32; i++) {
+        x = x << 1;
+        x += n % 2;
+        n = n >> 1;
+      }
+      return x;
+    }
+};
