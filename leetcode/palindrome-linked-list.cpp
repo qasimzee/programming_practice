43cc92b96e7fe1182b9ffe57a4812c6124802125
Qasim Zeeshan <newbillgates@gmail.com>
2016-02-03 07:33:49 +0500

    adding https://leetcode.com/problems/palindrome-linked-list/

---
+++ leetcode/palindrome-linked-list.cpp
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/palindrome-linked-list/
+/**
+  * Definition for singly-linked list.
+  *  * struct ListNode {
+  *   *     int val;
+  *    *     ListNode *next;
+  *     *     ListNode(int x) : val(x), next(NULL) {}
+  *      * };
+  *       */
+class Solution {
+  public:
+    bool isPalindrome(ListNode* head) {
+      int n = 0;
+      if (head == NULL) return true;
+      ListNode *node = head;
+      while (node != NULL) {
+        n++;
+        node = node->next;
+      }
+      stack<ListNode*> stk;
+      node = head;
+      int i = 0;
+      while (i < n/2) {
+        stk.push(node);
+        node = node->next;
+        i++;
+      }
+      if (n % 2 == 1)
+        node = node->next;
+      while (node != NULL) {
+        ListNode *p_node = stk.top();
+        stk.pop();
+        if (p_node->val != node->val)
+          return false;
+        node = node->next;
+      }
+      return true;
+    }
+};
