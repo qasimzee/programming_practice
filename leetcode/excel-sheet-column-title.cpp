c925d759c36b3f585f087dd6647d355d48fa529c
Qasim Zeeshan <newbillgates@gmail.com>
2016-02-29 17:53:47 -0800

    adding https://leetcode.com/problems/excel-sheet-column-title/

---
+++ leetcode/excel-sheet-column-title.cpp
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/excel-sheet-column-title/
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+string convertToTitle(int n) {
+  char map[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+  string str = "";
+  while (n > 0) {
+    n--;
+    str = map[n % 26] + str;
+    n /= 26;
+  }
+  return str;
+}
+
+int main() {
+  cout << convertToTitle(1);
+  return 0;
+}
+
+
+
