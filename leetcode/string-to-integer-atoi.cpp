642e9dfcb768d81d8607dd407a0556ee08156c7a
Qasim Zeeshan <newbillgates@gmail.com>
2016-03-21 07:58:39 +0700

    https://leetcode.com/problems/string-to-integer-atoi/

---
+++ leetcode/string-to-integer-atoi.cpp
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/string-to-integer-atoi/
+class Solution {
+  public:
+    int myAtoi(string str) {
+      int i = 0;
+      bool sign = false;
+      int ret = 0;
+      while (str[i] == ' ') {
+        i++;
+      }
+      if (str[i] == '-') {
+        sign = true;
+        i++;
+      }
+      else if (str[i] == '+') {
+        sign = false;
+        i++;
+      }
+      int prev_ret = 0;
+      while (i < str.size() && str[i] <= '9' && str[i] >= '0') {
+        prev_ret = ret;
+        ret *= 10;
+        ret += (str[i] - '0');
+        if (ret < prev_ret) {
+          if (sign) return INT_MIN;
+          return INT_MAX;
+        }
+        i++;
+      }
+      if (sign)
+        ret *= -1;
+      return ret;
+    }
+};
