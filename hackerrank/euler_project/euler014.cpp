88c49fe9acf0fcca514686a25abaad79612dba9e
Qasim Zeeshan <newbillgates@gmail.com>
2016-05-30 07:07:00 +0700

    adding memoization to https://www.hackerrank.com/contests/projecteuler/challenges/euler014/submissions/code/5966254

--- hackerrank/euler_project/euler014.cpp
+++ hackerrank/euler_project/euler014.cpp
@@ -11,15 +11,14 @@
 
 using namespace std;
 
+int mem[5000001] = {0};
+
 int findTerms(int n)
 {
   int count = 1;
   while (n > 1)
   {
-    if (n % 2 == 0)
-      n = n / 2;
-    else
-      n = 3 * n + 1;
+    n = n % 2 == 0 ? n >> 1 : 3 * n + 1;
     count++;
   }
   return count;
@@ -28,14 +27,21 @@ int findTerms(int n)
 int findMaxTerms(int n)
 {
   int max_terms = 0;
+  int ret = 1;
   for (int i = 2; i <= n; i++)
   {
-    int terms = findTerms(i);
-    cout <<"Terms: "<< terms << endl;
-    if (terms > max_terms)
+    int terms;
+    if (mem[i] == 0)
+      terms = mem[i] = findTerms(i);
+    else
+      terms = mem[i];
+    if (terms >= max_terms)
+    {
       max_terms = terms;
+      ret = i;
+    }
   }
-  return max_terms;
+  return ret;
 }
 
 int main() {
