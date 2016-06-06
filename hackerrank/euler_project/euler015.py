e013e874dc286547e0b2fb154a89bb352a129f01
Qasim Zeeshan <newbillgates@gmail.com>
2016-06-07 05:11:01 +0700

    adding solution for https://www.hackerrank.com/contests/projecteuler/challenges/euler015

---
+++ hackerrank/euler_project/euler015.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+def factorial(n):
+	x = 1;
+	for i in range(1, n + 1):
+		x *= i
+	return x
+
+
+def computePaths(a, b):
+	return (factorial(a + b) / (factorial(a) * factorial(b))) % (1000000000 + 7);
+
+
+T = int(raw_input())
+while (T > 0):
+	str = raw_input().split(' ') 
+	a = int(str[0])
+	b = int(str[1])
+	print computePaths(a, b)
+	T = T - 1
+	
