c2179571f058dedc1e0245286e4a0afe40db565d
Qasim Zeeshan <newbillgates@gmail.com>
2016-06-19 13:55:21 +0700

    adding https://www.hackerrank.com/contests/projecteuler/challenges/euler020

---
+++ hackerrank/euler_project/euler020.py
@@ -0,0 +1,16 @@
+def factorialDigitSum(N):
+  factorial = 1;
+  for x in range(1, N + 1):
+    factorial *= x
+  sum = 0
+  while factorial > 0:
+    sum  = sum + factorial % 10
+    factorial /= 10
+    return sum
+
+T = int(raw_input())
+for x in range(0, T):
+    N = int(raw_input())
+    print factorialDigitSum(N)
+
+
