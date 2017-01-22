4c34f474e9265434f54d39510b8fbd8d24943839
Qasim Zeeshan <newbillgates@gmail.com>
2017-01-22 14:00:14 +0700

    accepted class Solution { public:     int numberOfBoomerangs(vector<pair<int, int>>& points) {         int count = 0;         for (int i = 0; i < points.size(); i++) {             unordered_map<double, int> ctr(points.size());             for (int j = 0; j < points.size(); j++) {                 count += 2 * ctr[hypot(points[i].first - points[j].first, points[i].second - points[j].second)]++;             }         }         return count;     } };

--- leetcode/number-of-boomerangs.cpp
+++ leetcode/number-of-boomerangs.cpp
@@ -1,20 +1,12 @@
-//NOT ACCEPTED: https://leetcode.com/problems/number-of-boomerangs/
+//https://leetcode.com/problems/number-of-boomerangs/
 class Solution {
   public:
-    double pointDistance(pair<int, int> p1, pair<int, int> p2) {
-      return (p1.first - p2.first) * (p1.first - p2.first) + (p1.second - p2.second) * (p1.second - p2.second);
-    }
     int numberOfBoomerangs(vector<pair<int, int>>& points) {
       int count = 0;
       for (int i = 0; i < points.size(); i++) {
+        unordered_map<double, int> ctr(points.size());
         for (int j = 0; j < points.size(); j++) {
-          for (int k = 0; k < points.size(); k++) {
-            if (i != j && i != k && j != k) {
-              double dis1 = pointDistance(points[i], points[j]);
-              double dis2 = pointDistance(points[j], points[k]);
-              count += (dis1 == dis2);
-            }
-          }
+          count += 2 * ctr[hypot(points[i].first - points[j].first, points[i].second - points[j].second)]++;
         }
       }
       return count;
